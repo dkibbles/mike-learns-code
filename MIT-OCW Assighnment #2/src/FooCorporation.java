3deab017725d6891bf499d4724c3efe8a823d3d7
MikeLautensack <110644605+MikeLautensack@users.noreply.github.com>
2022-08-14 22:35:18 -0400

    Update FooCorporation.java

--- MIT-OCW Assighnment #2/src/FooCorporation.java
+++ MIT-OCW Assighnment #2/src/FooCorporation.java
@@ -1,5 +1,5 @@
-
 public class FooCorporation {
+
 	double basePay;
 	double hrsWorked;
 	double totalPay;
@@ -7,28 +7,33 @@ public class FooCorporation {
 	double overtimeHrs;
 	
 	public FooCorporation(double basePay, double hrsWorked) {
+	
 		this.basePay = basePay;
-		this.hrsWorked = hrsWorked;
-		
+		this.hrsWorked = hrsWorked;	
 	}
 				
-	public void payCalc(double basePay, double hrsWorked) {		
+	public void payCalc(double basePay, double hrsWorked) {	
+	
 		overtimeHrs = (hrsWorked - 40);
 		
 		if (hrsWorked > 40 && basePay > 8.00) {
+		
 			totalPay = (((hrsWorked - overtimeHrs) * basePay) + (overtimeHrs * (basePay * 1.5)));
 			System.out.println(totalPay);
+			
 		} else if (hrsWorked <= 40 && basePay > 8.00) {
+		
 			totalPay = (hrsWorked * basePay);
 			System.out.println(totalPay);
+			
 		} else {
-			System.out.println("Error");
-		}
 		
+			System.out.println("Error");
+			
+		}
 	}
-	
-
 	public static void main(String[] args) {
+	
 		FooCorporation employee1 = new FooCorporation(7.50,35);
 		FooCorporation employee2 = new FooCorporation(8.20,47);
 		FooCorporation employee3 = new FooCorporation(10.00,73);
@@ -37,5 +42,4 @@ public class FooCorporation {
 		employee2.payCalc(8.20,47);
 		employee3.payCalc(10.00,73);
 	}
-
 }
