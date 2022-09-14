0d2ddab8ad6c1a6d3028287156d7db4f0339a822
dkibbles <dkibblesdkibbles@gmail.com>
2022-09-13 20:07:43 -0400

    Part way done

--- Projects/Calculator/src/Calculator.java
+++ Projects/Calculator/src/Calculator.java
@@ -2,6 +2,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.HashMap;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -12,34 +13,47 @@ public class Calculator implements ActionListener {
 
 	JFrame frame;
 	JTextField displayWindow;
+	JPanel buttonPanel, displayWindowPanel;
+
+
 	JButton b1,b2,b3,b4,b5,b6,b7,b8,b9,b0;
 	JButton equals, clear, add, sub, mul, div, dec;
 	JButton settings;
-	JPanel buttonPanel, displayWindowPanel;
+	
+
+	HashMap<String,JButton> inputs = new HashMap<String, JButton>();
+
+	String[] CALC_BUTTONS = {"0","1","2","3","4","5","6","7","8","9","=","clr","+","-","*","/",".","S"};
 
 	public Calculator() {
 
 		frame = new JFrame("Calculator");
 		buttonPanel = new JPanel();
 		displayWindow = new JTextField();
-		b1 = new JButton("1");
-		b2 = new JButton("2");
-		b3 = new JButton("3");
-		b4 = new JButton("4");
-		b5 = new JButton("5");
-		b6 = new JButton("6");
-		b7 = new JButton("7");
-		b8 = new JButton("8");
-		b9 = new JButton("9");
-		b0 = new JButton("0");
-		equals = new JButton("=");
-		clear = new JButton("clr");
-		add = new JButton("+");
-		sub = new JButton("-");
-		mul = new JButton("*");
-		div = new JButton("/");
-		dec = new JButton(".");
-		settings = new JButton("S");
+
+		//So you can declare a 
+		// b1 = new JButton("1");
+		// b2 = new JButton("2");
+		// b3 = new JButton("3");
+		// b4 = new JButton("4");
+		// b5 = new JButton("5");
+		// b6 = new JButton("6");
+		// b7 = new JButton("7");
+		// b8 = new JButton("8");
+		// b9 = new JButton("9");
+		// b0 = new JButton("0");
+		// equals = new JButton("=");
+		// clear = new JButton("clr");
+		// add = new JButton("+");
+		// sub = new JButton("-");
+		// mul = new JButton("*");
+		// div = new JButton("/");
+		// dec = new JButton(".");
+		// settings = new JButton("S");
+
+		for(int i = 0; i < CALC_BUTTONS.length; i++) {
+			inputs.put(CALC_BUTTONS[i], new JButton(CALC_BUTTONS[i]));
+		}
 		
 		frame.getContentPane().setPreferredSize(new Dimension(295,304));
 		frame.pack();
@@ -55,86 +69,90 @@ public class Calculator implements ActionListener {
 		buttonPanel.setBackground(Color.decode("#343b4a"));
 		buttonPanel.setLayout(null);
 
-		buttonPanel.add(b7);
-		buttonPanel.add(b8);
-		buttonPanel.add(b9);
-		buttonPanel.add(div);
+		// buttonPanel.add(b7);
+		// buttonPanel.add(b8);
+		// buttonPanel.add(b9);
+		// buttonPanel.add(div);
 
-		buttonPanel.add(b4);
-		buttonPanel.add(b5);
-		buttonPanel.add(b6);
-		buttonPanel.add(mul);
+		// buttonPanel.add(b4);
+		// buttonPanel.add(b5);
+		// buttonPanel.add(b6);
+		// buttonPanel.add(mul);
 
-		buttonPanel.add(b1);
-		buttonPanel.add(b2);
-		buttonPanel.add(b3);
-		buttonPanel.add(sub);
+		// buttonPanel.add(b1);
+		// buttonPanel.add(b2);
+		// buttonPanel.add(b3);
+		// buttonPanel.add(sub);
 
-		buttonPanel.add(b0);
-		buttonPanel.add(dec);
-		buttonPanel.add(add);
+		// buttonPanel.add(b0);
+		// buttonPanel.add(dec);
+		// buttonPanel.add(add);
 
-		buttonPanel.add(equals);
-		buttonPanel.add(clear);
+		// buttonPanel.add(equals);
+		// buttonPanel.add(clear);
 		
-		frame.add(settings);
+		inputs.forEach((name, button) -> buttonPanel.add(button));
 
 
-		settings.setBounds(270, 5, 20, 20);
-		settings.setBackground(Color.decode("#eedeb5"));
+
+		frame.add(inputs.get("S"));
+
+
+		inputs.get("S").setBounds(270, 5, 20, 20);
+		inputs.get("S").setBackground(Color.decode("#eedeb5"));
 		
 		displayWindow.setBounds(40, 25, 215, 34);
 
-		b1.setBounds(10, 88, 60, 34);
-		b1.setBackground(Color.decode("#eedeb5"));
+		inputs.get("1").setBounds(10, 88, 60, 34);
+		inputs.get("1").setBackground(Color.decode("#eedeb5"));
 
-		b2.setBounds(75, 88, 60, 34);
-		b2.setBackground(Color.decode("#eedeb5"));
+		inputs.get("2").setBounds(75, 88, 60, 34);
+		inputs.get("2").setBackground(Color.decode("#eedeb5"));
 
-		b3.setBounds(140, 88, 60, 34);
-		b3.setBackground(Color.decode("#eedeb5"));
+		inputs.get("3").setBounds(140, 88, 60, 34);
+		inputs.get("3").setBackground(Color.decode("#eedeb5"));
 
-		b4.setBounds(10, 49, 60, 34);
-		b4.setBackground(Color.decode("#eedeb5"));
+		inputs.get("4").setBounds(10, 49, 60, 34);
+		inputs.get("4").setBackground(Color.decode("#eedeb5"));
 
-		b5.setBounds(75, 49, 60, 34);
-		b5.setBackground(Color.decode("#eedeb5"));
+		inputs.get("5").setBounds(75, 49, 60, 34);
+		inputs.get("5").setBackground(Color.decode("#eedeb5"));
 
-		b6.setBounds(140, 49, 60, 34);
-		b6.setBackground(Color.decode("#eedeb5"));
+		inputs.get("6").setBounds(140, 49, 60, 34);
+		inputs.get("6").setBackground(Color.decode("#eedeb5"));
 
-		b7.setBounds(10, 10, 60, 34);
-		b7.setBackground(Color.decode("#eedeb5"));
+		inputs.get("7").setBounds(10, 10, 60, 34);
+		inputs.get("7").setBackground(Color.decode("#eedeb5"));
 
-		b8.setBounds(75, 10, 60, 34);
-		b8.setBackground(Color.decode("#eedeb5"));
+		inputs.get("8").setBounds(75, 10, 60, 34);
+		inputs.get("8").setBackground(Color.decode("#eedeb5"));
 
-		b9.setBounds(140, 10, 60, 34);
-		b9.setBackground(Color.decode("#eedeb5"));
+		inputs.get("9").setBounds(140, 10, 60, 34);
+		inputs.get("9").setBackground(Color.decode("#eedeb5"));
 
-		b0.setBounds(10, 127, 60, 34);
-		b0.setBackground(Color.decode("#eedeb5"));
+		inputs.get("0").setBounds(10, 127, 60, 34);
+		inputs.get("0").setBackground(Color.decode("#eedeb5"));
 
-		equals.setBounds(10, 166, 255, 34);
-		equals.setBackground(Color.decode("#eedeb5"));
+		inputs.get("=").setBounds(10, 166, 255, 34);
+		inputs.get("=").setBackground(Color.decode("#eedeb5"));
 
-		clear.setBounds(140, 127, 60, 34);
-		clear.setBackground(Color.decode("#eedeb5"));
+		inputs.get("clr").setBounds(140, 127, 60, 34);
+		inputs.get("clr").setBackground(Color.decode("#eedeb5"));
 
-		add.setBounds(205, 127, 60, 34);
-		add.setBackground(Color.decode("#eedeb5"));
+		inputs.get("+").setBounds(205, 127, 60, 34);
+		inputs.get("+").setBackground(Color.decode("#eedeb5"));
 
-		sub.setBounds(205, 88, 60, 34);
-		sub.setBackground(Color.decode("#eedeb5"));
+		inputs.get("-").setBounds(205, 88, 60, 34);
+		inputs.get("-").setBackground(Color.decode("#eedeb5"));
 
-		mul.setBounds(205, 49, 60, 34);
-		mul.setBackground(Color.decode("#eedeb5"));
+		inputs.get("*").setBounds(205, 49, 60, 34);
+		inputs.get("*").setBackground(Color.decode("#eedeb5"));
 
-		div.setBounds(205, 10, 60, 34);
-		div.setBackground(Color.decode("#eedeb5"));
+		inputs.get("/").setBounds(205, 10, 60, 34);
+		inputs.get("/").setBackground(Color.decode("#eedeb5"));
 
-		dec.setBounds(75, 127, 60, 34);
-		dec.setBackground(Color.decode("#eedeb5"));
+		inputs.get(".").setBounds(75, 127, 60, 34);
+		inputs.get(".").setBackground(Color.decode("#eedeb5"));
 
 
 	}
