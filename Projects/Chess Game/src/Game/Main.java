50423457a88fbc64d65fe8c2fa0f2099963fb6ac
Mike <mikelautensack100@gmail.com>
2022-10-02 01:33:46 -0400

    working on chess app and learning a lot

--- Projects/Chess Game/src/Game/Main.java
+++ Projects/Chess Game/src/Game/Main.java
@@ -1,8 +1,14 @@
 package Game;
 
+import board.Board;
+
 public class Main {
     
     public static void main(String[] args) {
+
+        Board board = new Board();
+
+        board.printBoard();
 
     }
 }
--- Projects/Chess Game/src/board/Board.java
+++ Projects/Chess Game/src/board/Board.java
@@ -2,10 +2,28 @@ package board;
 
 public class Board {
 
-    Square[][] board;
+    Square[][] board = new Square[8][8];
 
-    Board() {
+    public Board() {
 
+        for(int i = 0; i < board.length; i++) {
+
+            squareColor color = (i % 1 == 0) ? squareColor.LIGHT : squareColor.DARK;
+
+            for(File file : File.values()) {
+
+                Square square= new Square(false, color, new squareCoordinates(file, -i + 9));
+                color = (color == squareColor.DARK) ? squareColor.LIGHT : squareColor.DARK;
+            }
+        }
     }
     
+    public void printBoard() {
+        for(Square[] row : board) {
+            for(Square square : row) {
+                System.out.println(square);
+            }
+            System.out.println();
+        }
+    }
 }
