5b681c9f74d6fbc1777c97b5dde8f08eb72a9837
Mike <mikelautensack100@gmail.com>
2022-10-20 16:16:32 -0400

    trying to figure out javaFX

--- Projects/Chess Game/src/GUI/App.java
+++ Projects/Chess Game/src/GUI/App.java
@@ -1,7 +1,11 @@
 package GUI;
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -10,29 +14,19 @@ import javafx.stage.Stage;
 public class App extends Application {
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+  Parent root;
+try {
+    root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+    Scene scene = new Scene(root);
+
   
-  /*
-  Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
-  Scene scene = new Scene(root);
-  */
-  
-  Scene scene = new Scene(root, 300, 250);
-  
-  primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.show();
+} catch (IOException e) {
+}
+  
     }
  
  public static void main(String[] args) {
---
+++ Projects/Chess Game/src/GUI/MainController.java
@@ -0,0 +1,13 @@
+package GUI;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class MainController {
+
+    @FXML
+    void Test(ActionEvent event) {
+        System.out.println("test");
+    }
+
+}
