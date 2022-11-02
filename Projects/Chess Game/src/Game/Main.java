b830f25d1deb399b0cb05d3c6e67df5d0ed0ea5a
Mike <mikelautensack100@gmail.com>
2022-11-01 22:44:26 -0400

    refactored the gui package into the game package

--- Projects/Chess Game/src/Game/Main.java
+++ Projects/Chess Game/src/Game/Main.java
@@ -1,14 +1,38 @@
 package Game;
 
 import board.Board;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application{
+
+    @Override
+    public void start(Stage primaryStage) { 
+        Parent root;
+try {
+    root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+    Scene scene = new Scene(root);
+
+  
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+} catch (Exception e) {
+    System.out.println(e);
+}
+  
+    }
     
     public static void main(String[] args) {
 
         Board board = new Board();
 
         board.printBoard();
+
+        launch(args);
 
     }
 }
--- Projects/Chess Game/src/Game/MainController.java
+++ Projects/Chess Game/src/Game/MainController.java
@@ -1,4 +1,4 @@
-package GUI;
+package Game;
 
 import java.io.IOException;
 
